4098506bef940d5ec08beb83484f037865872bda
ray <audi3m@naver.com>
2025-01-12 23:50:16 +0900

    Feat: 도메인 책 구조체

--- CleanShopping/Domain/Entities/Book.swift
+++ CleanShopping/Domain/Entities/Book.swift
@@ -1,5 +1,5 @@
 //
-//  Item.swift
+//  Book.swift
 //  CleanShopping
 //
 //  Created by J Oh on 1/3/25.
@@ -7,7 +7,15 @@
 
 import Foundation
 
-struct Item: Identifiable {
+struct Book: Identifiable {
     let id = UUID()
-    
+    let title: String
+    let link: String
+    let image: String
+    let author: String
+    let discount: String
+    let publisher: String
+    let pubdate: Date
+    let isbn: Int
+    let description: String
 }
