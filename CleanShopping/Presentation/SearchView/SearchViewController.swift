fa41e5d62b5e24f25909ac74fd0a6106ec6c599b
ray <audi3m@naver.com>
2025-01-14 23:22:38 +0900

    Feat: 검색창 일부 구현

--- CleanShopping/Presentation/SearchView/SearchViewController.swift
+++ CleanShopping/Presentation/SearchView/SearchViewController.swift
@@ -11,34 +11,57 @@ import SnapKit
 final class SearchViewController: UIViewController {
     
     // 검색창, 검색 결과 리스트, 상세화면
-    private let searchBar: UISearchBar = {
+    private lazy var searchBar: UISearchBar = {
         let searchBar = UISearchBar()
+        searchBar.backgroundImage = UIImage()
+        searchBar.placeholder = "검색해보세요..."
+        searchBar.delegate = self
         return searchBar
     }()
     
     private let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: SearchViewController.layout)
         return collectionView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureHierarchy()
+        configureUI()
     }
 
 }
 
+extension SearchViewController: UISearchBarDelegate {
+    
+}
+
 extension SearchViewController {
     
-    private func configureConstraints() {
-        
+    private func configureHierarchy() {
+        view.addSubview(searchBar)
+        view.addSubview(collectionView)
     }
     
     private func configureUI() {
+        searchBar.snp.makeConstraints { make in
+            
+        }
         
+        collectionView.snp.makeConstraints { make in
+            
+        }
     }
     
     
+}
+
+extension SearchViewController {
+    static let layout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 100, height: 100)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        return layout
+    }()
 }
