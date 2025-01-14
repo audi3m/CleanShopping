d46b6772fdb7a59977cbabd1c746540b6f04094c
ray <audi3m@naver.com>
2025-01-14 23:22:13 +0900

    Feat: 책 결과 셀 레이아웃 구현

--- CleanShopping/Presentation/SearchView/SearchedBookCell.swift
+++ CleanShopping/Presentation/SearchView/SearchedBookCell.swift
@@ -11,19 +11,73 @@ import Kingfisher
 
 final class SearchedBookCell: UICollectionViewCell {
     
-    private let bookImageView: UIImageView = {
+    private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         return imageView
     }()
-    
     private let titleLabel: UILabel = {
         let label = UILabel()
         return label
     }()
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    private let priceLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureUI()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
+}
+
+extension SearchedBookCell {
+    private func configureHierarchy() {
+        let views = [imageView, titleLabel, infoLabel, priceLabel]
+        views.forEach { contentView.addSubview($0) }
+    }
     
+    private func configureUI() {
+        imageView.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview().inset(10)
+            make.leading.equalToSuperview().inset(10)
+            // 너비
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(10)
+            make.leading.equalTo(imageView.snp.trailing).offset(20)
+            make.trailing.equalToSuperview().inset(10)
+        }
+        
+        infoLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel).offset(10)
+            make.leading.equalTo(imageView).offset(20)
+            make.trailing.equalToSuperview().inset(10)
+        }
+        
+        priceLabel.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(10)
+            make.bottom.equalToSuperview().inset(10)
+        }
+            
+    }
+    
+    func configureData(book: Book) {
+        imageView.kf.setImage(with: URL(string: book.image))
+        titleLabel.text = book.title
+        infoLabel.text = book.info
+        priceLabel.text = book.price
+    }
 }
