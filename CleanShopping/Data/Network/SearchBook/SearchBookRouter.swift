00066e46460d020851fb20143e32084b6b65af54
ray <audi3m@naver.com>
2025-01-17 23:39:03 +0900

    Add: 파라미터 요청 이름 변경 적용, switch default 대신 케이스로 구현

---
+++ CleanShopping/Data/Network/SearchBook/SearchBookRouter.swift
@@ -0,0 +1,89 @@
+//
+//  SearchBookRouter.swift
+//  CleanShopping
+//
+//  Created by J Oh on 1/16/25.
+//
+
+import Foundation
+import Alamofire
+
+enum SearchBookRouter {
+    case naver(param: NaverBookRequestParameters)
+    case kakao(param: KakaoBookRequestParameters)
+}
+
+extension SearchBookRouter: TargetType {
+    var baseURL: String {
+        switch self {
+        case .naver:
+            return BookAPI.naver.url
+        case .kakao:
+            return BookAPI.kakao.url
+        }
+    }
+    
+    var method: Alamofire.HTTPMethod {
+        switch self {
+        case .naver:
+            return .get
+        case .kakao:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .naver:
+            return ""
+        case .kakao:
+            return ""
+        }
+    }
+    
+    var headers: Alamofire.HTTPHeaders {
+        switch self {
+        case .naver:
+            return [
+                NaverBookAPI.idHeader: NaverBookAPI.id,
+                NaverBookAPI.secretHeader: NaverBookAPI.secret
+            ]
+        case .kakao:
+            return [
+                KakaoBookAPI.header: KakaoBookAPI.auth
+            ]
+        }
+    }
+    
+    var queryItems: [URLQueryItem]? {
+        switch self {
+        case .naver(let param):
+            let parameters = [
+                URLQueryItem(name: "query", value: param.query),
+                URLQueryItem(name: "display", value: "\(param.display)"),
+                URLQueryItem(name: "start", value: "\(param.start)"),
+                URLQueryItem(name: "sort", value: param.sort.rawValue)
+            ]
+            return parameters
+        case .kakao(let param):
+            let parameters = [
+                URLQueryItem(name: "query", value: param.query),
+                URLQueryItem(name: "sort", value: param.sort.rawValue),
+                URLQueryItem(name: "page", value: "\(param.page)"),
+                URLQueryItem(name: "size", value: "\(param.size)"),
+//                URLQueryItem(name: "target", value: param.target.rawValue)
+            ]
+            return parameters
+        }
+    }
+    
+    var body: Data? {
+        switch self {
+        case .naver:
+            return nil
+        case .kakao:
+            return nil
+        }
+    }
+    
+}
