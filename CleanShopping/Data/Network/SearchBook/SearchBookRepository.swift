2674164ce4941d562a035c9c458f0d1dc6732161
ray <audi3m@naver.com>
2025-02-01 00:17:29 +0900

    Feat: api별 네트워크 요청 레포지터리 구현

--- CleanShopping/Data/Network/SearchBook/SearchBookRepository.swift
+++ CleanShopping/Data/Network/SearchBook/SearchBookRepository.swift
@@ -19,30 +19,31 @@ final class SearchBookRepository {
 
 extension SearchBookRepository {
     
-    func searchKakaoBook(bookRequest: BookRequest) {
-        let params = KakaoBookRequestParameters(query: bookRequest.query, page: bookRequest.page)
-        networkManager.request(target: .kakao(param: params),
-                               of: KakaoBookResponseDTO.self) { result in
-            switch result {
-            case .success(let success):
-                print(success)
-            case .failure(let failure):
-                print(failure)
+    func searchBook(bookRequest: BookRequest) {
+        switch bookRequest.api {
+        case .naver:
+            let params = bookRequest.toDTO() as! NaverBookRequestParameters
+            networkManager.request(target: .naver(param: params),
+                                   of: NaverBookResponseDTO.self) { result in
+                switch result {
+                case .success(let value):
+                    print(value.toDomain())
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        case .kakao:
+            let params = bookRequest.toDTO() as! KakaoBookRequestParameters
+            networkManager.request(target: .kakao(param: params),
+                                   of: KakaoBookResponseDTO.self) { result in
+                switch result {
+                case .success(let value):
+                    print(value.toDomain())
+                case .failure(let error):
+                    print(error)
+                }
             }
         }
-    }
-    
-    func searchNaverBook(bookRequest: BookRequest) {
-        let params = NaverBookRequestParameters(query: bookRequest.query, start: bookRequest.page)
-        networkManager.request(target: .naver(param: params),
-                               of: NaverBookResponseDTO.self) { result in
-            switch result {
-            case .success(let success):
-                print(success)
-            case .failure(let failure):
-                print(failure)
-            }
-        }
-    }
+    } 
     
 }
