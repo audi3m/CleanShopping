b150a7636bf771a1774c36ea685cec006cd4e385
ray <audi3m@naver.com>
2025-02-01 00:16:53 +0900

    Feat: 네트워크 매니저 구현

--- CleanShopping/Data/Network/SearchBook/SearchBookTools/BookNetworkManager.swift
+++ CleanShopping/Data/Network/SearchBook/SearchBookTools/BookNetworkManager.swift
@@ -9,58 +9,29 @@ import Foundation
 import Alamofire
 
 final class BookNetworkManager {
-    
     static let shared = BookNetworkManager()
     private init() { }
+}
+    
+extension BookNetworkManager {
     
     func request<T: Decodable>(target: SearchBookRouter,
                                of type: T.Type,
                                handler: @escaping (Result<T, Error>) -> Void) {
-        
-        
-        
+        do {
+            let request = try target.asURLRequest()
+            AF.request(request)
+                .responseDecodable(of: T.self) { response in
+                    switch response.result {
+                    case .success(let value):
+                        handler(.success(value))
+                    case .failure(let error):
+                        handler(.failure(error))
+                    }
+                }
             
+        } catch {
+            print(error)
         }
-    
+    }
 }
-
-//extension BookNetworkManager {
-//    
-//    func searhNaverBooks(params: NaverBookRequestParameters,
-//                         handler: @escaping (Result<NaverBookResponseDTO, NaverBookResponseError>) -> Void) {
-//        do {
-//            let request = try SearchBookRouter.naver(param: params).asURLRequest()
-//            AF.request(request)
-//                .validate(statusCode: 200...299)
-//                .responseDecodable(of: NaverBookResponseDTO.self) { response in
-//                    switch response.result {
-//                    case .success(let value):
-//                        handler(.success(value))
-//                    case .failure:
-//                        handler(.failure(.responseError))
-//                    }
-//                }
-//        } catch {
-//            print(error)
-//        }
-//    }
-//    
-//    func searhKakaoBooks(params: KakaoBookRequestParameters,
-//                         handler: @escaping (Result<KakaoBookResponseDTO, KakaoBookResponseError>) -> Void) {
-//        do {
-//            let request = try SearchBookRouter.kakao(param: params).asURLRequest()
-//            AF.request(request)
-//                .validate(statusCode: 200...299)
-//                .responseDecodable(of: KakaoBookResponseDTO.self) { response in
-//                    switch response.result {
-//                    case .success(let value):
-//                        handler(.success(value))
-//                    case .failure(let error):
-//                        handler(.failure(.responseError))
-//                    }
-//                }
-//        } catch {
-//            print(error)
-//        }
-//    }
-//}
