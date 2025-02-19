e9568668d5b674c9009304625d32279793948f00
ray <audi3m@naver.com>
2025-02-19 23:20:58 +0900

    Feat: RxDataSource 섹션 구분 구현

--- CleanShopping/Presentation/SearchTab/View/Components/SearchTabSection.swift
+++ CleanShopping/Presentation/SearchTab/View/Components/SearchTabSection.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import RxDataSources
 
 enum SearchBookSection: Int, CaseIterable {
   case filter
@@ -15,4 +16,31 @@ enum SearchBookSection: Int, CaseIterable {
 enum SearchBookSectionItem: Hashable {
   case filterOption(BookAPI)
   case listData(Book)
+}
+
+
+// RxDataSource
+struct SearchBookHeaderSectionModel {
+  var api: BookAPI
+}
+
+struct SearchBookBodySectionModel {
+  var books: [Book]
+}
+
+enum SearchBookSectionModelType {
+  case header(SearchBookHeaderSectionModel)
+  case body(SearchBookBodySectionModel)
+}
+
+struct SearchBookSectionModel {
+  var items: [SearchBookSectionModelType]
+}
+
+extension SearchBookSectionModel: SectionModelType {
+  init(original: SearchBookSectionModel, items: [SearchBookSectionModelType]) {
+    self = original
+    self.items = items
+  }
+  
 }
