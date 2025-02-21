64167cc141bf66fc7e77ad1bf77884e6249d4f59
ray <audi3m@naver.com>
2025-02-21 22:44:18 +0900

    Feat: RxDataSource 섹션 아이템, 섹션 모델 열거형 구현, SectionModelType 프로토콜 적용

--- CleanShopping/Presentation/SearchTab/View/Components/SearchTabSection.swift
+++ CleanShopping/Presentation/SearchTab/View/Components/SearchTabSection.swift
@@ -8,39 +8,45 @@
 import Foundation
 import RxDataSources
 
-enum SearchBookSection: Int, CaseIterable {
-  case filter
-  case list
-}
-
 enum SearchBookSectionItem: Hashable {
   case filterOption(BookAPI)
   case listData(Book)
 }
 
+enum SearchBookSection: Int, CaseIterable {
+  case filter
+  case list
+}
+
+
 
 // RxDataSource
-struct SearchBookHeaderSectionModel {
-  var api: BookAPI
+
+enum SearchBookSectionItem2 {
+//  case header(model: BookAPI)
+  case body(model: Book)
 }
 
-struct SearchBookBodySectionModel {
-  var books: [Book]
-}
-
-enum SearchBookSectionModelType {
-  case header(SearchBookHeaderSectionModel)
-  case body(SearchBookBodySectionModel)
-}
-
-struct SearchBookSectionModel {
-  var items: [SearchBookSectionModelType]
+enum SearchBookSectionModel {
+//  case headerSection(items: BookAPI.AllCases)
+  case bodySection(items: [SearchBookSectionItem2])
 }
 
 extension SearchBookSectionModel: SectionModelType {
-  init(original: SearchBookSectionModel, items: [SearchBookSectionModelType]) {
+  
+  typealias Item = SearchBookSectionItem2
+  
+  var items: [SearchBookSectionItem2] {
+    switch self {
+//    case .headerSection(let apis):
+//      return apis
+    case .bodySection(let books):
+      return books
+    }
+  }
+  
+  init(original: SearchBookSectionModel, items: [SearchBookSectionItem2]) {
     self = original
-    self.items = items
   }
   
 }
