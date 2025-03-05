2421d6d67ad8211659461445a4e33e74789519d3
ray <audi3m@naver.com>
2025-03-05 11:33:43 +0900

    Feat: 검색탭 뷰모델 생성

--- CleanShopping/Presentation/Screens/SearchTab/SearchBookViewModel2.swift
+++ CleanShopping/Presentation/Screens/SearchTab/SearchBookViewModel2.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import RxSwift
+
+final class SearchBookViewModel2 {
+  
+  private let searchBookUseCase: SearchBookUseCase
+  private let saveBookUseCase: SaveBookUseCase
+  private let disposeBag = DisposeBag()
+  
+  init(searchBookUseCase: SearchBookUseCase, saveBookUseCase: SaveBookUseCase) {
+    self.searchBookUseCase = searchBookUseCase
+    self.saveBookUseCase = saveBookUseCase
+  }
+  
+  struct Input {
+    
+  }
+  
+  struct Output {
+    
+  }
+  
+  
+}
+
+extension SearchBookViewModel2 {
+  func transform(input: Input) -> Output {
+    return Output()
+  }
+}
+
+// search
+extension SearchBookViewModel2 {
+  
+}
+
+// save
+extension SearchBookViewModel2 {
+  
+}
+
+extension SearchBookViewModel2 {
+  
+}
+
+extension SearchBookViewModel2 {
+  
+}
