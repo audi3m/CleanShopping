1bc3a7a240246afc459390de327d8ad6f8ef0933
ray <audi3m@naver.com>
2025-03-10 22:47:26 +0900

    Feat: 도서 네트워크 매퍼 구현

--- CleanShopping/Data/DataRepository/BookMapper.swift
+++ CleanShopping/Data/DataRepository/BookMapper.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-struct BookMapper {
+// Local DB
+struct LocalBookMapper {
   static func toDTO(_ book: Book) -> LocalBookModel {
     return LocalBookModel(title: book.title,
                           link: book.link,
@@ -32,4 +33,45 @@ struct BookMapper {
                 isbn: localBook.isbn,
                 description: localBook.desc)
   }
+}
+
+// Network
+struct NetworkBookMapper {
+  static func toDomainResponse(response: NaverBookResponseDTO) -> BookResponse {
+    return BookResponse(totalCount: response.total,
+                        books: response.items.map { self.toDomainBook(dto: $0) },
+                        isEnd: response.total <= response.start + response.display)
+  }
+  
+  static func toDomainResponse(response: KakaoBookResponseDTO) -> BookResponse {
+    return BookResponse(totalCount: response.meta.totalCount,
+                        books: response.documents.map { self.toDomainBook(dto: $0) },
+                        isEnd: response.meta.isEnd)
+  }
+  
+  static func toDomainBook(dto: NaverBookDTO) -> Book {
+    return Book(title: dto.title,
+                link: dto.link,
+                image: dto.image,
+                author: dto.author,
+                discount: dto.discount,
+                publisher: dto.publisher,
+                pubdate: dto.pubdate,
+                isbn: dto.isbn,
+                description: dto.description)
+  }
+  
+  static func toDomainBook(dto: KakaoDocumentDTO) -> Book {
+    return Book(title: dto.title,
+                link: dto.url,
+                image: dto.thumbnail,
+                author: dto.authors.joined(separator: ", "),
+                discount: String(dto.price),
+                publisher: dto.publisher,
+                pubdate: dto.datetime,
+                isbn: dto.isbn,
+                description: dto.contents)
+  }
+  
+  
 }
