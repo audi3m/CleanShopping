991e6954a89459b74a92a0aec75d015e58c7953a
ray <audi3m@naver.com>
2025-03-11 22:28:13 +0900

    Feat: 검색레포지터리 구현체 구현

--- CleanShopping/Data/DataRepository/SearchBookRepositoryImpl.swift
+++ CleanShopping/Data/DataRepository/SearchBookRepositoryImpl.swift
@@ -8,111 +8,51 @@
 import Foundation
 import RxSwift
 
+enum BookRequestError: Error {
+  case badRequest
+}
+
 final class SearchBookRepositoryImpl: SearchBookRepository {
   
   private let dataSource: SearchBookDataSource
-  private let networkManager: BookNetworkManager
   
-  init(dataSource: SearchBookDataSource, networkManager: BookNetworkManager) {
+  init(dataSource: SearchBookDataSource) {
     self.dataSource = dataSource
-    self.networkManager = networkManager
   }
   
-  func searchBook() {
-    dataSource.searchBooks()
-  }
-  
-  func searchBookSingle() {
-    dataSource.searchBooks()
-  }
-  
-  func searchBookAsync() {
-    dataSource.searchBooks()
-  }
-  
-}
-
-extension SearchBookRepositoryImpl {
-  
-  func searchBookSingle(bookRequest: BookRequest) -> Single<Result<BookResponse, BookRequestError>> {
-    return Single.create { [weak self] single -> Disposable in
-      guard let self else {
-        single(.success(.failure(.badRequest)))
-        return Disposables.create()
+  func searchBook(bookRequest: BookRequest, handler: @escaping (Result<BookResponse, Error>) -> Void) {
+    dataSource.searchBook(bookRequest: bookRequest) { result in
+      switch result {
+      case .success(let response):
+        handler(.success(response.toDomain()))
+      case .failure(let error):
+        print(error.localizedDescription)
       }
-      
-      switch bookRequest.api {
-      case .naver:
-        let params = bookRequest.toDTO() as! NaverBookRequestParameters
-        self.networkManager.request(target: .naver(param: params), of: NaverBookResponseDTO.self) { result in
-          switch result {
-          case .success(let value):
-            single(.success(.success(value.toDomain())))
-          case .failure(let error):
-            single(.failure(error))
-          }
-        }
-        
-      case .kakao:
-        let params = bookRequest.toDTO() as! KakaoBookRequestParameters
-        self.networkManager.request(target: .kakao(param: params), of: KakaoBookResponseDTO.self) { result in
-          switch result {
-          case .success(let value):
-            single(.success(.success(value.toDomain())))
-          case .failure(let error):
-            single(.failure(error))
-          }
-        }
-      }
-      
-      return Disposables.create()
     }
   }
-}
-
-// @escaping
-extension SearchBookRepositoryImpl {
   
-  func searchBook(bookRequest: BookRequest, handler: @escaping (Result<BookResponse, BookRequestError>) -> Void) {
-    switch bookRequest.api {
-    case .naver:
-      let params = bookRequest.toDTO() as! NaverBookRequestParameters
-      networkManager.request(target: .naver(param: params), of: NaverBookResponseDTO.self) { result in
+  func searchBookSingle(bookRequest: BookRequest) -> Single<BookResponse> {
+    return dataSource.searchBookSingle(bookRequest: bookRequest)
+      .flatMap { result -> Single<BookResponse> in
         switch result {
-        case .success(let value):
-          handler(.success(value.toDomain()))
-        case .failure:
-          handler(.failure(.badRequest))
+        case .success(let response):
+          if let naverResponse = response as? NaverBookResponseDTO {
+            return .just(NetworkBookMapper.toDomainResponse(response: naverResponse))
+          } else if let kakaoResponse = response as? KakaoBookResponseDTO {
+            return .just(NetworkBookMapper.toDomainResponse(response: kakaoResponse))
+          } else {
+            return .error(BookRequestError.badRequest)
+          }
+        case .failure(let error):
+          return .error(error)
         }
       }
-    case .kakao:
-      let params = bookRequest.toDTO() as! KakaoBookRequestParameters
-      networkManager.request(target: .kakao(param: params), of: KakaoBookResponseDTO.self) { result in
-        switch result {
-        case .success(let value):
-          handler(.success(value.toDomain()))
-        case .failure:
-          handler(.failure(.badRequest))
-        }
-      }
-    }
   }
   
-}
- 
-// async/await
-extension SearchBookRepositoryImpl {
-  func searchBookAsync(bookRequest: BookRequest) async throws -> BookResponse {
-    switch bookRequest.api {
-    case .naver:
-      let params = bookRequest.toDTO() as! NaverBookRequestParameters
-      let response = try await networkManager.requestAsync(target: .naver(param: params), of: NaverBookResponseDTO.self)
-      return response.toDomain()
-    case .kakao:
-      let params = bookRequest.toDTO() as! KakaoBookRequestParameters
-      let response = try await networkManager.requestAsync(target: .kakao(param: params), of: KakaoBookResponseDTO.self)
-      return response.toDomain()
-    }
+  func searchBookAsync(bookRequest: BookRequest) {
+    
   }
+  
+  
   
 }
