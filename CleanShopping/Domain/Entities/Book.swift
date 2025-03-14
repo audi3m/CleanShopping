fe1dbdc0a6844c9188baa12fa92222499b0867a1
ray <audi3m@naver.com>
2025-03-14 22:37:41 +0900

    Feat: 생성자 구현, 데이터소스 활용을 위한 IdentifiableType 채택

--- CleanShopping/Domain/Entities/Book.swift
+++ CleanShopping/Domain/Entities/Book.swift
@@ -6,8 +6,10 @@
 //
 
 import Foundation
+import Differentiator
 
-struct Book: Identifiable, Hashable {
+struct Book: Identifiable, Hashable, IdentifiableType, Equatable {
+  
   let id = UUID()
   let title: String
   let link: String
@@ -18,6 +20,32 @@ struct Book: Identifiable, Hashable {
   let pubdate: String
   let isbn: String
   let description: String
+  
+  init(id: UUID = UUID(),
+       title: String,
+       link: String,
+       image: String,
+       author: String,
+       discount: String?,
+       publisher: String,
+       pubdate: String,
+       isbn: String,
+       description: String) {
+    self.id = id
+    self.title = title
+    self.link = link
+    self.image = image
+    self.author = author
+    self.discount = discount
+    self.publisher = publisher
+    self.pubdate = pubdate
+    self.isbn = isbn
+    self.description = description
+  }
+  
+  var identity: UUID {
+    return self.id
+  }
 }
 
 extension Book {
