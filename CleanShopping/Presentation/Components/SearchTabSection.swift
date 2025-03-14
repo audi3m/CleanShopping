7a6a98b5385fd8a09d573004a979fa8bd68dc364
ray <audi3m@naver.com>
2025-03-14 22:40:00 +0900

    Feat: 섹션 모델 AnimatableSectionModelType으로 변경, IdentifiableType 채택

--- CleanShopping/Presentation/Components/SearchTabSection.swift
+++ CleanShopping/Presentation/Components/SearchTabSection.swift
@@ -18,13 +18,21 @@ enum SearchBookSection: Int, CaseIterable {
   case list
 }
 
-
-
+// Model 2
 // RxDataSource
-enum SearchBookSectionItem2 {
+enum SearchBookSectionItem2: IdentifiableType, Equatable {
   case headerItem(BookAPI)
   case bodyItem(Book)
-//  case thirdSection
+  //  case thirdSection(Model)
+  
+  var identity: String {
+    switch self {
+    case .headerItem(let api):
+      return api.identity
+    case .bodyItem(let book):
+      return book.identity.uuidString
+    }
+  }
 }
 
 struct SearchBookSectionModel2 {
@@ -32,9 +40,13 @@ struct SearchBookSectionModel2 {
   var items: [SearchBookSectionItem2]
 }
 
-extension SearchBookSectionModel2: SectionModelType {
+extension SearchBookSectionModel2: AnimatableSectionModelType {
   init(original: SearchBookSectionModel2, items: [SearchBookSectionItem2]) {
     self = original
     self.items = items
+  }
+  
+  var identity: String {
+    return header
   }
 }
