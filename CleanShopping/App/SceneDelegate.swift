572bd24861f58f47a4108a46ba9af6e44805c387
ray <audi3m@naver.com>
2025-03-15 21:30:34 +0900

    Feat: DIContainer injecy

--- CleanShopping/App/SceneDelegate.swift
+++ CleanShopping/App/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   
   var window: UIWindow?
   
-  
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -20,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let scene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: scene)
     
-    let vc = HomeTabBarController()
+    let vc = HomeTabBarController(container: DIContainer.shared)
     //        let nav = UINavigationController(rootViewController: vc)
     window?.rootViewController = vc
     window?.makeKeyAndVisible()
