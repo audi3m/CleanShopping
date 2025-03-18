c54b54816813ce6a038fa3db2377d20c7512d8e1
ray <audi3m@naver.com>
2025-03-18 22:38:57 +0900

    Feat: 디테일뷰 시작

--- CleanShopping/Presentation/Screens/DetailView/DetailViewController.swift
+++ CleanShopping/Presentation/Screens/DetailView/DetailViewController.swift
@@ -11,6 +11,29 @@ import Kingfisher
 
 final class DetailViewController: BaseViewController {
   
+  private let imageView: UIImageView = {
+    let view = UIImageView()
+    return view
+  }()
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  private let priceLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  private let descLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  
+  private let viewModel = DetailViewViewModel()
+  
+//  let book = Book(title: <#T##String#>, link: <#T##String#>, image: <#T##String#>,
+//                  author: <#T##String#>, discount: <#T##String?#>, publisher: <#T##String#>,
+//                  pubdate: <#T##String#>, isbn: <#T##String#>, description: <#T##String#>)
+  
   // 표지
   // 제목
   // 가격
@@ -18,8 +41,43 @@ final class DetailViewController: BaseViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    rxBind()
+    makeNavigationItems()
   }
   
+  override func setHierarchy() {
+    
+  }
   
+  override func setLayout() {
+    
+  }
+  
+  override func setUI() {
+    
+  }
+  
+}
+
+// Rx
+extension DetailViewController {
+  private func rxBind() {
+    
+  }
+}
+
+extension DetailViewController {
+  
+  private func makeNavigationItems() {
+    let item = UIBarButtonItem(image: UIImage(systemName: "heart"),
+                               style: .plain, target: self,
+                               action: #selector(dummyFunction))
+    
+    navigationItem.leftBarButtonItems = [item]
+  }
+  
+  @objc private func dummyFunction() {
+    
+  }
   
 }
