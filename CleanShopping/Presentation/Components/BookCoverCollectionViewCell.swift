c2731e4f2ef13c92db195e977b142bc4261ff178
ray <audi3m@naver.com>
2025-03-26 00:23:37 +0900

    Feat: 저장 도서 목록에서 사용할 셀 구현

--- CleanShopping/Presentation/Components/BookCoverCollectionViewCell.swift
+++ CleanShopping/Presentation/Components/BookCoverCollectionViewCell.swift
@@ -5,4 +5,55 @@
 //  Created by J Oh on 3/25/25.
 //
 
-import Foundation
+import UIKit
+import Kingfisher
+import SnapKit
+
+final class BookCoverCollectionViewCell: UICollectionViewCell {
+  static let id = "BookCoverCollectionViewCell"
+  
+  private let imageView: UIImageView = {
+    let view = UIImageView()
+    view.contentMode = .scaleAspectFill
+    view.layer.masksToBounds = true
+    view.layer.cornerRadius = 8
+    return view
+  }()
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 11, weight: .semibold)
+    return label
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configureHierarchy()
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
+
+extension BookCoverCollectionViewCell {
+  private func configureHierarchy() {
+    contentView.addSubview(imageView)
+    contentView.addSubview(titleLabel)
+  }
+  
+  private func configureUI() {
+    imageView.snp.makeConstraints { make in
+      
+    }
+    titleLabel.snp.makeConstraints { make in
+      
+    }
+  }
+  
+  func configureData(book: Book) {
+    let coverUrl = URL(string: book.image)
+    imageView.kf.setImage(with: coverUrl)
+  }
+}
