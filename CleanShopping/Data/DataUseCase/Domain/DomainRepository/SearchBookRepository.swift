f01415794aa291e9a3d2699e6c8a333b404b8d49
ray <audi3m@naver.com>
2025-06-05 19:31:22 +0900

    Feat: 청소

--- CleanShopping/Data/DataUseCase/Domain/DomainRepository/SearchBookRepository.swift
+++ CleanShopping/Data/DataUseCase/Domain/DomainRepository/SearchBookRepository.swift
@@ -9,7 +9,5 @@ import Foundation
 import RxSwift
 
 protocol SearchBookRepository {
-//  func searchBook(bookRequest: BookRequest, handler: @escaping (Result<BookResponse, Error>) -> Void)
   func searchBookSingle(bookRequest: BookRequest) -> Single<Result<BookResponse, BookRequestError>>
-//  func searchBookAsync(bookRequest: BookRequest)
 }
